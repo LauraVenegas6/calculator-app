7655f52338b41a9791cf46e91bf181217b2bbd47
Laura Venegas <aleja09venegas@gmail.com>
2026-09-09 11:05:00 -0500

    chore: initialize backend

---
+++ backend/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(map[string]string{
+		"status": "ok",
+	})
+}
+
+func main() {
+	http.HandleFunc("/health", healthHandler)
+
+	log.Println("Server running on http://localhost:8080")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
